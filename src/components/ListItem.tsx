7f1b0eff40eb97b32cfc472a2a8b3b56a26997c5
Andika Bahari <kzlfxx@gmail.com>
2022-03-24 16:12:28 +0700

    Fix

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 interface ListItemProps {
   item: {
@@ -9,5 +10,29 @@ interface ListItemProps {
 }
 
 export const ListItem: React.FC<ListItemProps> = ({item}) => {
-  return <Text>{item.text}</Text>;
+  return (
+    <TouchableOpacity style={styles.listItem}>
+      <View style={styles.listItemView}>
+        <Text style={styles.listItemText}>{item.text}</Text>
+        <Icon name="remove" size={20} color="firebrick" />
+      </View>
+    </TouchableOpacity>
+  );
 };
+
+const styles = StyleSheet.create({
+  listItem: {
+    padding: 15,
+    backgroundColor: '#f8f8f8',
+    borderBottomWidth: 1,
+    borderColor: '#eee',
+  },
+  listItemView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  listItemText: {
+    fontSize: 16,
+  },
+});
